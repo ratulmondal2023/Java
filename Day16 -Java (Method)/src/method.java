05e58763a811b2370a3f17d6743099a563b3e9b9
Ratul Mondal <150381310+ratulmondal2023@users.noreply.github.com>
2024-06-21 01:28:41 +0600

    Add files via upload

---
+++ Day16 -Java (Method)/src/method.java
@@ -0,0 +1,17 @@
+public class method {
+    public static void main(String[] args) throws Exception {
+        int a,b;
+        a = 10;
+        b = 20;
+        sum(a, b);
+        /*int k = sum(a,b);
+        System.out.println(k);
+        k = sum(50,60);
+        System.out.println(k);*/
+    }
+    static void sum(int a, int b) {
+        int x = a + b;
+         System.out.println(x);
+        // return x;
+    }
+}
